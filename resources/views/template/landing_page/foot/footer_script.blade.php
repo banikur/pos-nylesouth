7b6645bf1ca515622d745b0d83014355c8e0422f
bani <banikur2103@gmail.com>
2021-05-16 05:36:46 +0700

    Landing Page Tenplate

---
+++ resources/views/template/landing_page/foot/footer_script.blade.php
@@ -0,0 +1,11 @@
+<!-- Javascripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="{{url('landing_page/js/jquery-1.9.1.min.js')}}"></script>
+
+<script src="{{url('landing_page/js/bootstrap.min.js')}}"></script>
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+<script src="{{url('landing_page/js/jquery.fitvids.js')}}"></script>
+<script src="{{url('landing_page/js/jquery.sequence-min.js')}}"></script>
+<script src="{{url('landing_page/js/jquery.bxslider.js')}}"></script>
+<script src="{{url('landing_page/js/main-menu.js')}}"></script>
+<script src="{{url('landing_page/js/template.js')}}"></script>
---
+++ resources/views/template/landing_page/head/header.blade.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    
+    <link rel="stylesheet" href="{{url('landing_page/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{url('landing_page/css/icomoon-social.css')}}">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,600,800' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <!--[if lte IE 8]>
+	    <link rel="stylesheet" href="css/leaflet.ie.css" />
+	<![endif]-->
+    <link rel="stylesheet" href="{{url('landing_page/css/main-orange.css')}}">
+
+    <script src="{{url('landing_page/js/modernizr-2.6.2-respond-1.1.0.min.js')}}"></script>
+</head>
