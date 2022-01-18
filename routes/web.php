c220c01e1bc785596a92377381dca0fd10d15ed4
Brayen Prayoga <brayenprayoga41@gmail.com>
2022-01-18 12:13:44 +0700

    tes update

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+ 
 Route::get('/', 'VerifiedController@index')->name('/');
 Route::get('/detail-produk', 'VerifiedController@detail_index')->name('detail');
 Route::get('/testapi', 'VerifiedController@testapi')->name('testapi');
